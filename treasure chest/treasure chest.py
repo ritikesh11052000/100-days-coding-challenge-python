065962683f916653e2126e82b8a33b3268508b9d
Ritikesh Machhewar <100903614+ritikesh11052000@users.noreply.github.com>
2024-03-23 12:08:50 +0000

    Your commit message

---
+++ treasure chest/treasure chest.py
@@ -0,0 +1,22 @@
+
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input("where you want to put 'x' e.g (a1,a2,a3)(b1,b2,b2,b3)(c1,c2,c3) : ") # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+# Your code below
+letter = position[0].lower()
+abc = ["a", "b", "c"]
+letter_index = abc.index(letter)
+number_index = int(position[1]) - 1
+map[number_index][letter_index] = "X"
+
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
+
+
+
